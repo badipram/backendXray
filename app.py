c645b7fb4853f5db8535beaf7a5b5f9dc12df2a4
badi amnu <badipram23@outlook.com>
2025-08-13 17:03:57 +0700

    edit port

--- app.py
+++ app.py
@@ -73,5 +73,9 @@ def predict():
         print("❌ Error during prediction:", e)
         return jsonify({"error": str(e)}), 500
 
+# if __name__ == "__main__":
+#     app.run(debug=True, port=5050)
+
 if __name__ == "__main__":
-    app.run(debug=True, port=5050)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
